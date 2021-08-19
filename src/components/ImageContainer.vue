b02a071d80ebb2889a705c5a7896cf1879067690
MAMUGI1492 <mmg1492@gmail.com>
2021-08-19 10:03:10 +0200

    💄 Refactor(ImageContainer): Improved interface

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,6 +1,12 @@
 <template>
   <q-card class="image-container" bordered flat>
     <q-card-section>
+      <div class="text-h6">Document detected:</div>
+    </q-card-section>
+
+    <q-separator dark inset />
+
+    <q-card-section>
       <q-img :src="src" fit="contain" width="300px" />
     </q-card-section>
   </q-card>
